40e212f0f168f193d67812e84e0ab26c0d758300
goutamprajapat554@gmail.com <goutamprajapat554@gmail.com>
2022-05-18 16:32:47 +0530

    first

---
+++ main.js
@@ -0,0 +1,6 @@
+//after window is loaded completely 
+window.onload = function() {
+  //hide the preloader
+  //use this -> for onload
+  // document.querySelector(".preloader").style.display = "none";
+}
